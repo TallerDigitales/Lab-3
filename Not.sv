9c4c8b9ef2657311a39fedd3989a3b50871e43f5
crisrivlop <crisrivlop@gmail.com>
2018-08-29 09:23:18 +0000

    Change Not.sv to Notr.sv

--- Not.sv
+++
@@ -1,5 +0,0 @@
-module Not #(parameter bus = 32) (input logic [bus-1:0]a,output logic [bus-1:0] s);
-
-	assign s = ~a;
-
-endmodule
---
+++ Notr.sv
@@ -0,0 +1,5 @@
+module Notr #(parameter bus = 32) (input logic [bus-1:0]a,output logic [bus-1:0] s);
+
+	assign s = ~a;
+
+endmodule
