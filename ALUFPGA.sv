414d5d25c578acb62fb59e858e28f04f8ba16216
Criss <mcxcrissx@gmail.com>
2018-08-30 17:34:00 -0600

    Corrección bandera CNVZ (de CVNZ)

--- ALUFPGA.sv
+++ ALUFPGA.sv
@@ -1,8 +1,7 @@
 module ALUFPGA(input logic [2:0] a, b, input logic [3:0] ALUFUN, output logic [6:0] out, output logic [3:0] CNVZ);
 	
 	logic [2:0] outAlu;
-	ALU #(3) alu (a, b, ALUFUN, outAlu, CVNZ);
-	
+	ALU #(3) alu (a, b, ALUFUN, outAlu, CNVZ);
 	
 	sevenSegments({1'b0, outAlu[2:0]}, out);
 	
